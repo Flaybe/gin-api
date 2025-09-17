fcc1a73a01782608910a895c0f1157734eb28ac1
Flaybe <mateo.schutt@gmail.com>
2025-09-17 22:38:11 +0300

    small fix

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"log"
-
 	"gin-api/config"
 	"gin-api/internal/api"
+	"log"
 )
 
 func main() {
 	log.Printf("Starting server in %s mode on %s\n", config.Env, config.ServerPort)
 
-	r := api.NewRouter()
-	if err := r.Run(config.ServerPort); err != nil {
+	// Initialize and run the server
+	router := api.NewRouter()
+	if err := router.Run(config.ServerPort); err != nil {
 		log.Fatalf("server exited: %v", err)
 	}
 }
