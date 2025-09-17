053992d710dcf309e2030d9e6269e80eed0e9352
Flaybe <mateo.schutt@gmail.com>
2025-09-17 22:29:35 +0300

    v1

---
+++ config/config.go
@@ -0,0 +1,16 @@
+package config
+
+import "time"
+
+// Some should variables should be in a .env but for clarity are placed here
+const (
+	ServerPort = ":8080"
+	// Simulated AI response latency
+	AILatency = 1 * time.Second
+	// timeout for /ask requests
+	DefaultTimeout = 5 * time.Second
+	// App environment: "dev" or "prod"
+	Env = "prod"
+	// Allowed origins (for CORS)
+	AllowedOrigins = "http://localhost:3000"
+)
